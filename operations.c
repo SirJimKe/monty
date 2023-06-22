8ae4b3dc929e7f84a1d1a756d1d1571ff0c634a7
Jimmy Mutuku <jimmymutuku1@gmail.com>
2023-06-22 18:35:51 +0000

    ✨✨✨ implemented mul, div, and mod operations

--- operations.c
+++ operations.c
@@ -28,7 +28,8 @@ void sub(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't sub, stack too short\n",
+			line_number);
 		exit(EXIT_FAILURE);
 	}
 
@@ -36,3 +37,71 @@ void sub(stack_t **stack, unsigned int line_number)
 	pop(stack, line_number);
 }
 
+/**
+ * div - divides the value of the top element by the second top element
+ * @stack: stack
+ * @line_number: line number
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n /= (*stack)->n;
+	pop(stack, line_number);
+}
+
+/**
+ * mul - multiplies the value of the top element by the second top element
+ * @stack: stack
+ * @line_number: line number
+ */
+
+void mul(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n *= (*stack)->n;
+	pop(stack, line_number);
+}
+
+
+/**
+ * mod - computes the remainder of the division of the second top element
+ * by the top element
+ * @stack: stack
+ * @line_number: line number
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n",
+			line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n %= (*stack)->n;
+	pop(stack, line_number);
+}
